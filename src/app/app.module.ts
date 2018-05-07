2b6d552a90e91074384639bf83938cdbd705798b
TioStalin <daniel.tapiara@sansano.usm.cl>
2018-05-07 20:09:25 -0300

    Login y bodega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterializeModule } from 'angular2-materialize';
 import { HomeComponent } from './home/home.component';
+import { BodegaComponent } from './bodega/bodega.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BodegaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
